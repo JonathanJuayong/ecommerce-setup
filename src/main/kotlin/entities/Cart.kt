fa81be1bce341ba6a9fd4545cf429c020301f314
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-12-16 23:33:43 +0800

    refactored cart methods and properties

--- src/main/kotlin/entities/Cart.kt
+++ src/main/kotlin/entities/Cart.kt
@@ -3,38 +3,50 @@ package entities
 import interfaces.CartInterface
 import loaders.ProductLoader
 
-class Cart: CartInterface {
-    override var cartProducts: MutableList<Product> = mutableListOf()
+class Cart : CartInterface {
     private val productsList = ProductLoader.products
-
     private var total = 0
+    private var cartItems: MutableList<Pair<Int, Int>> = mutableListOf() // Pair<productId, qty>
+
+    private fun getProductObjectById(productId: Int): Product =
+        productsList.find { it.productId == productId }!!
+
+    private fun updateCartItemQty(cartItem: Pair<Int, Int>, qty: Int) {
+        removeProduct(cartItem.first)
+        cartItems.add(cartItem.copy(second = cartItem.second + qty))
+    }
+
+    fun getCartItems() =
+        cartItems.map { (id, qty) -> Pair(getProductObjectById(id), qty) }
 
     //ADD TO CART
-    fun addProduct(productId: Int) {
-        val product = productsList.find { it.productId == productId }
-        if (product != null) {
-            cartProducts.add(product)
+    override fun addProductId(productId: Int, qty: Int) {
+        val cartItem = cartItems.find { (id, _) -> id == productId }
+        if (cartItem == null) {
+            cartItems.add(Pair(productId, qty))
         } else {
-            println("Product ID does not exist")
+            updateCartItemQty(cartItem, qty)
         }
     }
 
     //REMOVE TO CART
-    fun removeProduct(product: Product) {
-        cartProducts.remove(product)
+    override fun removeProduct(productId: Int) {
+        cartItems.removeIf { (id, _) -> id == productId }
     }
 
     //TOTAL CART
-    fun getTotal(): Int {
-        cartProducts.forEach() {
-            total += it.price
+    override fun getTotal(): Int {
+        var total = 0
+        val itemsWithQty = getCartItems()
+        itemsWithQty.forEach { (product, qty) ->
+            total += product.price * qty
         }
         return total
     }
 
     //RESET CART
-    fun resetCart() {
+    override fun resetCart() {
         total = 0
-        cartProducts.clear()
+        cartItems.clear()
     }
 }
--- src/main/kotlin/interfaces/CartInterface.kt
+++ src/main/kotlin/interfaces/CartInterface.kt
@@ -3,5 +3,8 @@ package interfaces
 import entities.Product
 
 interface CartInterface {
-    var cartProducts: MutableList<Product>
+    fun addProductId(productId: Int, qty: Int)
+    fun removeProduct(productId: Int)
+    fun getTotal(): Int
+    fun resetCart()
 }
