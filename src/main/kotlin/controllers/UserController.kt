2af55729a41e44206ab121ea914fef2d35a1c3d5
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-12-16 23:56:05 +0800

    created user controller and view

---
+++ src/main/kotlin/controllers/UserController.kt
@@ -0,0 +1,21 @@
+package controllers
+
+import entities.user.Member
+import entities.user.User
+
+class UserController {
+    private var user: User? = null
+    private var members: List<Member> = mutableListOf()
+
+    fun LogIn() {
+
+    }
+
+    fun SignUp() {
+
+    }
+
+    fun SignInAsGuest() {
+
+    }
+}
