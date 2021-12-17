2f01f54468469231440cd4fb496dbf2f1f924752
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-12-18 02:02:06 +0800

    added qty to product class

--- src/main/kotlin/entities/Product.kt
+++ src/main/kotlin/entities/Product.kt
@@ -5,6 +5,7 @@ import interfaces.ProductInterface
 data class Product(
     override val productName: String,
     override val price: Int,
-    override val productId: Int
+    override val productId: Int,
+    var qty: Int = 0
 ) : ProductInterface {
 }
